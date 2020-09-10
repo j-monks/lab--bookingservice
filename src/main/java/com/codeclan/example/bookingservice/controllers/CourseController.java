8397c6433008eb9dd5fdd8fb429374161e53c667
James Monks <J.monks23@gmail.com>
2020-09-10 13:15:49 +0100

    find by bookings customer name working for course

--- src/main/java/com/codeclan/example/bookingservice/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/bookingservice/controllers/CourseController.java
@@ -16,10 +16,15 @@ public class CourseController {
 
     @GetMapping(value = "/courses")
     public ResponseEntity getAllCoursesAndFilters(
-            @RequestParam(required = false, name = "rating") Integer rating) {
+            @RequestParam(required = false, name = "rating") Integer rating,
+            @RequestParam(required = false, name = "name") String name) {
         if (rating != null) {
             return new ResponseEntity(courseRepository.findByRating(rating), HttpStatus.OK);
         }
+        if (name != null) {
+            return new ResponseEntity(courseRepository.findByBookingsCustomerName(name), HttpStatus.OK);
+        }
+
         return new ResponseEntity(courseRepository.findAll(), HttpStatus.OK);
     }
 }
