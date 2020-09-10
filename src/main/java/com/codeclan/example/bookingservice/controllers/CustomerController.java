c9eb14874f07262e1c20d74878181a0ff05f1449
James Monks <J.monks23@gmail.com>
2020-09-10 13:31:52 +0100

    customers can be found for a given course name on the customer

--- src/main/java/com/codeclan/example/bookingservice/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/bookingservice/controllers/CustomerController.java
@@ -1,4 +1,27 @@
 package com.codeclan.example.bookingservice.controllers;
 
+import com.codeclan.example.bookingservice.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity getAllCustomersAndFilters(
+            @RequestParam(required = false, name = "name") String name
+    ){
+        if (name != null) {
+            return new ResponseEntity(customerRepository.findByBookingsCourseName(name), HttpStatus.OK);
+        }
+
+        return new ResponseEntity(customerRepository.findAll(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/codeclan/example/bookingservice/repositories/CustomerRepository.java
+++ src/main/java/com/codeclan/example/bookingservice/repositories/CustomerRepository.java
@@ -3,6 +3,9 @@ package com.codeclan.example.bookingservice.repositories;
 import com.codeclan.example.bookingservice.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    List<Customer> findByBookingsCourseName(String name);
 }
